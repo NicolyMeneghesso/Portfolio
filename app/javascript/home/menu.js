be8aedd09190ddcd9f6f7551fb9027d096850f46
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-04-26 23:42:23 -0300

    Initial commit of Rails Blog Project
    Initial commit of Rails Blog Project

---
+++ app/javascript/home/menu.js
@@ -0,0 +1,19 @@
+//CODIGO DO MENU TOGGLE
+document.addEventListener('DOMContentLoaded', function() {
+    let menuToggle = document.getElementById('menu-toggle')
+    let sidenav = document.getElementById("mySidenav")
+  
+    menuToggle.addEventListener('click', function() {
+        this.classList.toggle('change')
+        sidenav.style.display = "block"
+  
+        setTimeout(() => {
+          // Ajuste a largura do sidenav e o posicionamento do card
+          if (sidenav.style.width === "0px" || sidenav.style.width === "") {
+              sidenav.style.width = "850px" // Abre o sidenav
+          } else {
+              sidenav.style.width = "0px" // Fecha o sidenav
+          }
+        }, 100)
+    });
+});
