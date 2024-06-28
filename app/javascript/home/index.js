7cbf9feb019e5e6b9df8f146fca2b50e47a99582
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-06-27 22:13:44 -0300

    ajuste tela home

--- app/javascript/home/index.js
+++ app/javascript/home/index.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() { 
-    let img = document.getElementById("img-main-0");
-    let container = document.getElementById("container");
+    let img = document.getElementById('lamps')
+    let rowBody = document.getElementById('row-body')
+    let cardMain = document.getElementById('card-main')
+    let lightBeam = document.getElementById('light-beam')
+
+    // Mostrar o feixe de luz quando a página for carregada
+    lightBeam.classList.add('light-beam');
 
     img.addEventListener('click', function() {
-        container.style.transition = 'background-color 0.5s ease';
-        container.style.backgroundColor = 'rgb(0, 229, 255)'; // Cor de destino
+        rowBody.classList.toggle('row-body-moon') //toggle ele adiciona/remove a classe da lista
+        cardMain.classList.toggle('card-main-moon')
     });
 });
