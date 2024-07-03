739005aa89b1342785b15beaf9ab0aa81c8c3d18
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-07-02 21:43:09 -0300

    ajuste tela home

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,14 +6,3 @@ import "./home/menu.js"
 import "./home/about.js"
 import "./home/index.js"
 
-
-let monitorScreen = document.getElementById("monitorScreen");
-let randCaracters = ['a', 'b', 'c', 'd', 'e', 'x', '2', ' ', '@']
-let screenText = "Hello World!"
-setInterval(() => {
-    
-    let randChar = randCaracters[Math.floor(Math.random()*randCaracters.length)];
-    screenText += randChar;
-
-    monitorScreen.innerHTML = screenText;
-}, 100)
--- app/javascript/home/index.js
+++ app/javascript/home/index.js
@@ -13,4 +13,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
         lightBeam.classList.toggle('transparent')
     });
+
+
+    let monitorScreen = document.getElementById("monitorScreen");
+    let randCaracters = document.getElementById("randCaracters").innerHTML.trim().split("<br>");
+    let screenText = ""; // Inicializa screenText como uma string vazia
+    let currentIndex = 0; // Inicia no primeiro elemento da array
+
+    setTimeout(() => {  // Define um atraso antes de executar a função interna.
+        monitorScreen.innerHTML = ""; // Limpa o conteúdo atual do elemento monitorScreen.
+        let intervalId = setInterval(() => { // Inicia uma função para ser executada a cada milissegundos
+            if (currentIndex < randCaracters.length) { // Verifica se ainda há mais elementos na array para exibir
+                let randChar = randCaracters[currentIndex]; // Pega o elemento atual da array
+                screenText += randChar + "<br>";  // Adiciona o elemento atual e uma nova linha ao texto acumulado
+                monitorScreen.innerHTML = screenText; // Atualiza o conteúdo exibido na tela
+                currentIndex++; // Passa para o próximo elemento da array
+            } else {
+                clearInterval(intervalId); // Para quando todos os elementos forem exibidos
+            }
+        }, 2000);
+    }, 2000); // Define um atraso de 2 segundos antes de iniciar a exibição dos elementos da array
 });
