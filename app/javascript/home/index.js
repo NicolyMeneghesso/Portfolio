72a6ca554ce847a0860f1197d39bbf9a712eb6f4
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-07-11 21:22:32 -0300

    create page about
    Ajuste final na pagina home e inicio da criação da pagina about

--- app/javascript/home/index.js
+++ app/javascript/home/index.js
@@ -15,10 +15,6 @@ document.addEventListener('DOMContentLoaded', function() {
     })
 
         //código para interação do monitor
-        const codeLines = document.getElementById('codeLines');
-        const lines = codeLines.innerHTML.split('<br>'); // Divide o conteúdo inicial em linhas.
-
-        // Contém o texto que queremos simular a digitação
         const codeSnippet = [
             "function helloWorld() {",
             "  document.getElementById('hello-world').textContent =",
@@ -29,23 +25,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
         let currentLine = 0 
         let currentChar = 0 
-        const startingLine = 0; // Índice da linha inicial (10 corresponde ao índice 0 do array lines)
 
         function typeCode() {
             // Verifica se ainda há linhas do codeSnippet para digitar
             if (currentLine < codeSnippet.length) {
                 // Verifica se ainda há caracteres na linha atual para digitar
                 if (currentChar < codeSnippet[currentLine].length) {
-                    lines[startingLine + currentLine] += codeSnippet[currentLine][currentChar];
+                    const lineTextElement = document.getElementById(`line-${currentLine}`);
+                    lineTextElement.textContent += codeSnippet[currentLine][currentChar];
                     currentChar++;
                 } else {
                     currentLine++;
                     currentChar = 0;
                 }
-                codeLines.innerHTML = lines.join('<br>');
                 setTimeout(typeCode, 100);
             }
         }
-
+        
         typeCode();
 })
