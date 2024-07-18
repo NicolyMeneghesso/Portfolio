8526e18dd125b67f6dc6c14e4cf40e7939852d30
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-07-17 21:56:28 -0300

    ajustes ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+    def project_name(index)
+        if index == 1
+            "Spike"
+        elsif index == 2
+            "Projeto 2"
+        else
+            "Projeto #{index}"
+        end
+    end
+
 end
