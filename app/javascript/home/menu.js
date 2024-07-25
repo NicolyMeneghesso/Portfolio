33dffec2f2da427521a2f71c183f1608a6c934fb
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-07-25 19:52:35 -0300

    ajustes menu

--- app/javascript/home/menu.js
+++ app/javascript/home/menu.js
@@ -4,11 +4,12 @@ document.addEventListener('DOMContentLoaded', function () {
     // Alternar entre os modos claro e escuro ao clicar no checkbox
     day.addEventListener('change', function () {
         document.body.classList.toggle('body-sun')
-        
+        document.body.classList.toggle('light-mode');
+               
         let menu = document.getElementsByClassName('navbar')[0]
         menu.classList.toggle('menu-sun')
         
-        let navTextElements = document.querySelectorAll('.nav-text p')
+        let navTextElements = document.querySelectorAll('.nav-text')
         navTextElements.forEach(function (text) {
             text.classList.toggle('nav-text-sun')
         });
