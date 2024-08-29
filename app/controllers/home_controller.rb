99c5e1cc339074021715d297819c80eddd688d66
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-08-28 21:40:08 -0300

    ajuste responsividade

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,8 @@ class HomeController < ApplicationController
       { image: 'logo-css.png', alt: 'Logo-css', name: 'CSS' },
       { image: 'logo-js.png', alt: 'Logo-js', name: 'JavaScript' },
       { image: 'logo-bootstrap.png', alt: 'Logo-bootstrap', name: 'Bootstrap' },
-      { image: 'logo-autocad.png', alt: 'Logo-html', name: 'AutoCad' },
+      { image: 'logo-github.png', alt: 'Logo-github', name: 'GitHub' },
+      { image: 'logo-autocad.png', alt: 'Logo-autocad', name: 'AutoCad' },
     ]
   end
 end
