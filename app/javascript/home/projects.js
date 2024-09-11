5a2fbebb8574210a76340c3a3ad0690ee9519858
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2024-09-10 21:28:02 -0300

    create cards + modal
    Criação e ajuste de layout dos cards da tela de projetos, mais a criação do modal

---
+++ app/javascript/home/projects.js
@@ -0,0 +1,4 @@
+document.querySelector('.btn-projects').addEventListener('click', function() {
+    var myModal = new bootstrap.Modal(document.getElementById('projectModal'));
+    myModal.show();
+})
